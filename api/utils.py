65fffbc282fb6bae162d9abc16eb26d55a07240e
SnowMelody <45004592+SnowMelody@users.noreply.github.com>
2020-10-12 01:42:02 +0800

    Add files via upload

---
+++ api/utils.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, Blueprint, request, Response, jsonify
+import jwt
+from hashlib import pbkdf2_hmac
+import os
+
+JWT_SECRET_KEY="DanielTechTips!!CZ2006"
+
+def generate_hash(plain_password, password_salt):
+    password_hash = pbkdf2_hmac(
+        "sha256",
+        b"%b" % bytes(plain_password, "utf-8"),
+        b"%b" % bytes(password_salt, "utf-8"),
+        10000,
+    )
+    return password_hash.hex()
+
+def generate_jwt_token(content):
+    encoded_content = jwt.encode(content, JWT_SECRET_KEY, algorithm="HS256")
+    token = str(encoded_content).split("'")[1]
+    return token
+
+def generate_hash(plain_password, password_salt):
+    password_hash = pbkdf2_hmac(
+        "sha256",
+        b"%b" % bytes(plain_password, "utf-8"),
+        b"%b" % bytes(password_salt, "utf-8"),
+        10000,
+    )
+    return password_hash.hex()
+
+def generate_salt():
+    salt = os.urandom(16)
+    return salt.hex()
