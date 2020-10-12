67b38919204c8fdd3a3cb307ee43748199259351
gilbert-khonstantine <khonstantine@gmail.com>
2020-10-12 12:05:51 +0800

    bug fix

--- app.py
+++ app.py
@@ -84,7 +84,6 @@ class Item(db.Model):
     def __repr__(self):
         return '<Item %r>' % self.id
 
-db = SQLAlchemy(app)
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
