e97c3c425fc8b65bb858bdcf2175a8f44515108f
Snailoading <chuan_ann@hotmail.com>
2020-10-14 14:27:53 +0800

    verify email

--- api/verify.py
+++ api/verify.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+import re
 
 
 def check_email(new_email):
@@ -8,11 +9,25 @@ def check_email(new_email):
     #Check that email is empty
     if len(new_email) == 0:
         return False
+
+    #example@example.com || example@example.sg 
+    regex = '^[a-zA-Z0-9]+[\._]?[a-zA-Z0-9]+[@]\w+[.]\w{2,3}$'
+    #example@example.edu.sg || example@example.gov.sg
+    regexExtra = '^[a-zA-Z0-9]+[\._]?[a-zA-Z0-9]+[@]\w+[.]\w{2,3}[.]\w{2,3}$'
+    #NTU email
+    regexNTU = '^[a-zA-Z0-9]+@e\.ntu\.edu\.sg$'
+
+    check = re.search(regex, new_email)
+    checkExtra = re.search(regexExtra, new_email)
+    checkNTU = re.search(regexNTU, new_email)
+
+    if not check and not checkExtra and not checkNTU:
+        return False
     
     #Query to check if requested email change is already in use
     existing_email = Account.query.filter_by(email=new_email).first()
 
     if existing_email:
         return False
-    else:
-        return True
+    
+    return True
