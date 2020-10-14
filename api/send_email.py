c3bdbddd29ffdf77925356760ec44dfda158becc
Dixdy <nicholaslow21@yahoo.com.sg>
2020-10-14 15:36:50 +0800

    changed send email locations

--- api/send_email.py
+++ api/send_email.py
@@ -7,7 +7,7 @@ from app import mail
 
 def send_2fa():
     twoFA = ''.join(random.choices(string.ascii_uppercase + string.digits, k=3))
-    msg = Message('2FA', sender = 'danieltechtips2006@gmail.com', recipients = ['novaiscrap@gmail.com'])
+    msg = Message('2FA', sender = 'danieltechtips2006@gmail.com', recipients = ['danieltechtips2006@gmail.com'])
     msg.body = "Hello, this is your 2FA: " + twoFA
     mail.send(msg)
     return twoFA
