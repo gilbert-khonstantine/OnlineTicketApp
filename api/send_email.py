2b71dbb69e443f9948df5aecd6f5de59ce2a4c8c
Dixdy <nicholaslow21@yahoo.com.sg>
2020-10-14 16:01:32 +0800

    moved import line in send_email

--- api/send_email.py
+++ api/send_email.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_mail import Mail, Message
 import random
 import string
-from app import mail
 
 
 def send_2fa():
+    from app import mail
+    
     twoFA = ''.join(random.choices(string.ascii_uppercase + string.digits, k=3))
     msg = Message('2FA', sender = 'danieltechtips2006@gmail.com', recipients = ['danieltechtips2006@gmail.com'])
     msg.body = "Hello, this is your 2FA: " + twoFA
